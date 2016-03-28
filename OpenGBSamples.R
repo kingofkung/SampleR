3ccde89199b60b74269f44287027927260d3cdf3
Ben Rogers <bjroger@ku.edu>
2016-03-27 20:14:59 -0500

    Found file location. Now to figure out how to read it in...

--- OpenGBSamples.R
+++ OpenGBSamples.R
@@ -1,1 +1,5 @@
-## Open and store garage band samples,
+## Openand store garage band samples,
+loc <- "/Library/Audio/Apple Loops/Apple"
+setwd(loc)
+list.files()[1]
+setwd(paste(loc, list.files()[1], sep = "/"))
